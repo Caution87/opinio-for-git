4259af893784d00edcf831006dee7689bd50b582
Prajith <prajrocks2005@gmail.com>
2024-11-21 22:24:20 +0530

    Posting Comments

--- lib/pages/post_comments_page.dart
+++ lib/pages/post_comments_page.dart
@@ -12,10 +12,40 @@ class PostCommentsPage extends StatefulWidget {
   State<PostCommentsPage> createState() => _PostCommentsPageState();
 }
 
+final currentUser = FirebaseAuth.instance.currentUser!;
+//get access to firestore
+final FirestoreService firestoreService = FirestoreService();
+
+bool _isSelectedFor = false;
+bool _isSelectedAgainst = false;
+
 class _PostCommentsPageState extends State<PostCommentsPage> {
   final FirestoreService firestoreService = FirestoreService();
   //textController
   final commentController = TextEditingController();
+  void initState() {
+    super.initState();
+    //_isSelectedFor = widget.forOpinions.contains(currentUser.email!);
+    //_isSelectedAgainst = widget.againstOpinions.contains(currentUser.email!);
+  }
+
+  void toggleFor() {
+    setState(() {
+      _isSelectedFor = !_isSelectedFor;
+      if (_isSelectedAgainst) {
+        _isSelectedAgainst = false;
+      }
+    });
+  }
+
+  void toggleAgainst() {
+    setState(() {
+      _isSelectedAgainst = !_isSelectedAgainst;
+      if (_isSelectedFor) {
+        _isSelectedFor = false;
+      }
+    });
+  }
 
   //current user
   final currentUser = FirebaseAuth.instance.currentUser!;
@@ -40,6 +70,7 @@ class _PostCommentsPageState extends State<PostCommentsPage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text("Post Your Comment"),
@@ -47,7 +78,51 @@ class _PostCommentsPageState extends State<PostCommentsPage> {
       ),
       body: Column(
         children: [
-          Text(currentUser.email!),
+          //FOR OR AGAINST BUTTONS
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              GestureDetector(
+                onTap: () async => toggleFor(),
+                child: Container(
+                  width: screenWidth * 0.5,
+                  color: _isSelectedFor
+                      ? Color.fromRGBO(68, 161, 160, 1)
+                      : Color.fromRGBO(68, 161, 160, 0.4),
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "FOR",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () async => toggleAgainst(),
+                child: Container(
+                  width: screenWidth * 0.5,
+                  color: _isSelectedAgainst
+                      ? Color.fromRGBO(212, 77, 92, 1)
+                      : Color.fromRGBO(212, 77, 92, 0.4),
+                  child: Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Text(
+                        "AGAINST",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          //Text(currentUser.email!),
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Column(
